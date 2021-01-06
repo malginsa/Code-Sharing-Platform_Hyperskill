94d103cdda4ff061e3f7d17c70b7a97d3611822d
msa <msabh@yandex.ru>
2021-01-06 13:14:23 +0300

    stage4: spring boot actuator is added

--- src/main/java/platform/CodeRepository.java
+++ src/main/java/platform/CodeRepository.java
@@ -1,14 +1,8 @@
 package platform;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public interface CodeRepository extends CrudRepository<CodeSnippet, Integer> {
 
